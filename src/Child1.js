84f5073a3f8bc626942fa8ca115d6493c2911ece
abanoubm1234567 <amasoud1234567@gmail.com>
2024-12-05 21:53:53 -0500

    bad code

--- src/Child1.js
+++ src/Child1.js
@@ -8,57 +8,12 @@ class Child1 extends Component {
   componentDidMount() {}
 
   componentDidUpdate() {
-    var data = [
-      { radius: 12, category: 0 },
-      { radius: 4, category: 1 },
-      { radius: 19, category: 2 },
-      { radius: 9, category: 0 },
-      { radius: 7, category: 1 },
-      { radius: 14, category: 2 },
-      { radius: 5, category: 0 },
-      { radius: 23, category: 1 },
-      { radius: 16, category: 2 },
-      { radius: 2, category: 0 },
-      { radius: 25, category: 1 },
-      { radius: 11, category: 2 },
-      { radius: 6, category: 0 },
-      { radius: 20, category: 1 },
-      { radius: 18, category: 2 },
-      { radius: 13, category: 0 },
-      { radius: 24, category: 1 },
-      { radius: 10, category: 2 },
-      { radius: 8, category: 0 },
-      { radius: 22, category: 1 },
-      { radius: 17, category: 2 },
-      { radius: 3, category: 0 },
-      { radius: 21, category: 1 },
-      { radius: 15, category: 2 },
-      { radius: 1, category: 0 },
-      { radius: 16, category: 1 },
-      { radius: 5, category: 2 },
-      { radius: 9, category: 0 },
-      { radius: 7, category: 1 },
-      { radius: 19, category: 2 },
-      { radius: 10, category: 0 },
-      { radius: 13, category: 1 },
-      { radius: 14, category: 2 },
-      { radius: 6, category: 0 },
-      { radius: 12, category: 1 },
-      { radius: 23, category: 2 },
-      { radius: 25, category: 0 },
-      { radius: 11, category: 1 },
-      { radius: 8, category: 2 },
-      { radius: 20, category: 0 },
-      { radius: 18, category: 1 },
-      { radius: 15, category: 2 },
-      { radius: 1, category: 0 },
-      { radius: 17, category: 1 },
-      { radius: 2, category: 2 },
-    ];
+    var data = this.props.csv_data;
+    console.log(data);
     var colorScale = ["#66c2a5", "#fc8d62", "#8da0cb"];
     var catCenters = [100, 200, 300];
     d3.forceSimulation(data)
-      .force("y",d3.forceY((d) => catCenters[d.category])
+      .force("y",d3.forceY((d) => catCenters[d.Month == "March" ? 0 : (d.Month == "April" ? 1 : 2)])
       )
       .force(
         "collision",
